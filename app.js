9ce923cc1b35cbf00745201c038e0b6d232998c3
danilunac <182788341+danilunac@users.noreply.github.com>
2025-12-10 15:40:20 +0100

    feat: create a web page that generates a square grid of n divs

---
+++ app.js
@@ -0,0 +1,25 @@
+const container = document.querySelector('#container');
+
+const gridSize = 16;
+const cellSize = 960 / gridSize;
+
+function createCell(parentElement) {
+    const cell = document.createElement('div');
+    cell.style.border = '0.5px solid red';
+    cell.style.width = cellSize + 'px';
+    cell.style.height = cellSize + 'px';
+    cell.style.margin = '0';
+    cell.style.padding = '0';
+
+    parentElement.appendChild(cell);
+}
+
+function generateGrid(size) {
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            createCell(container);
+        }
+    }
+}
+
+generateGrid(gridSize);
