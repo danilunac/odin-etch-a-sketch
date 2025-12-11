31e34b4f31daef500c432234a3d2c7fe6fae7fb7
danilunac <182788341+danilunac@users.noreply.github.com>
2025-12-11 16:32:39 +0100

    feat: generate a random RGB color when the mouse hovers over a cell

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const container = document.querySelector('#container');
 
+function getRandomRGBColor() {
+    const red = Math.floor((Math.random() * 256));
+    const green = Math.floor((Math.random() * 256));
+    const blue = Math.floor((Math.random() * 256));
+
+    const rgbRandomColor = `rgb(${red}, ${green}, ${blue})`;
+
+    return rgbRandomColor;
+}
+
 function generateGrid() {  
     // Remove cells before generating new grid
     container.innerHTML = '';
@@ -15,7 +25,7 @@ function generateGrid() {
 
     function createCell(parentElement) {
         const cell = document.createElement('div');
-        cell.style.border = '0.5px solid grey';
+        cell.style.border = '0.1px solid grey';
         cell.style.width = cellSize + 'px';
         cell.style.height = cellSize + 'px';
         cell.style.margin = '0';
@@ -32,10 +42,12 @@ function generateGrid() {
 }
 
 // Create an event that changes the cell color on mouse hover
-container.addEventListener('mousemove', (e) => {
-    e.target.style.backgroundColor = 'blue';
-})
+container.addEventListener('mouseover', (e) => {
+    e.target.style.backgroundColor = getRandomRGBColor();
+});
 
 // Button that creates an event to generate a new grid
 const newGridBtn = document.querySelector('#newGridBtn');
 newGridBtn.addEventListener('click', generateGrid);
+
+
